5fd574b729dda37716258851c056361f4ca5cbeb
Selim DEMİR <selimdemir63151@gmail.com>
2026-05-18 22:35:37 +0300

    Proje dosyası ekle.

---
+++ AI_Smart_RAM_Manager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AI_Smart_RAM_Manager
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
