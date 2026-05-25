32b6847e25f9050454ebc58c2e2e68fe237a0f91
Selim DEMİR <selimdemir63151@gmail.com>
2026-05-25 19:24:37 +0300

    Added music

--- AI_Smart_RAM_Manager/MainWindow.xaml.cs
+++ AI_Smart_RAM_Manager/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Media;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace AI_Smart_RAM_Manager
 {
@@ -16,19 +18,42 @@ namespace AI_Smart_RAM_Manager
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public MainWindow()
+
+		private MediaPlayer myMediaPlayer = new MediaPlayer();
+        public MainWindow()
 		{
 			InitializeComponent();
-		}
+            
+        }
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			MessageBox.Show("Butona tıklandı.", "SA");
-		}
+			etiket.Content = textbox1.Text;
+			try
+			{
+				string executablePath = AppDomain.CurrentDomain.BaseDirectory;
+                string soundFilePath = System.IO.Path.Combine(executablePath, "anime-ahh.mp3");
+
+				myMediaPlayer.Open(new Uri(soundFilePath));
+				myMediaPlayer.Play();
+            }
+            catch (Exception ex)
+			{
+                MessageBox.Show("Ses çalınamadı: " + ex.Message);
+                return;
+            }
+        }
 
 		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
 		{
 
 		}
-	}
+
+
+
+
+
+
+
+    }
 }
